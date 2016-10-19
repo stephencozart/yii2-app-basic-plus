6562f960459d3e4082b35e5b10fcd81b2f296ee8
Kyle McCarthy <km@kylemccarthy.io>
2016-10-19 17:16:25 -0500

    remove composer initd cookie key

--- config/web.php
+++ config/web.php
@@ -12,7 +12,7 @@ $config = [
         ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '-Cmp1CW5ezznUQAxFRP-JGERdhaY4hwe',
+            'cookieValidationKey' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
